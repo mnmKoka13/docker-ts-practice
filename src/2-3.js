14d60828cce00778b906c10d2c3d55619dd504cd
Manami Kokado <kokado313.mnm@gmail.com>
2024-08-04 19:17:06 +0900

    typescript勉強 20240804

--- src/2-3.js
+++ src/2-3.js
@@ -1,4 +1,19 @@
 // 基本的な型の機能
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // 型推論
 function katasuiron() {
     var age = 10;
@@ -49,10 +64,142 @@ function kataAssertion() {
     // console.log(fuga.toFixed(2));
 }
 kataAssertion();
-function printPoint(point) {
-    console.log("x\u5EA7\u6A19\u306F ".concat(point.x, " \u3067\u3059"));
-    console.log("y\u5EA7\u6A19\u306F ".concat(point.y, " \u3067\u3059"));
+function kataAriase() {
+    function printPoint(point) {
+        console.log("x\u5EA7\u6A19\u306F ".concat(point.x, " \u3067\u3059"));
+        console.log("y\u5EA7\u6A19\u306F ".concat(point.y, " \u3067\u3059"));
+    }
+    printPoint({ x: 100, y: 200 });
+    function printName(firstName, formatter) {
+        console.log(formatter(firstName));
+    }
+    var upperCaseFormatter = function (name) { return name.toUpperCase(); };
+    printName('Michinobu', upperCaseFormatter);
+    // 上記エイリアスを指定して、
+    // 画面に表示する文字を定義するオブジェクトのキーと値を以下のように定義できる
+    var labels = {
+        topTitle: 'トップページのタイトルです',
+        topSubTitle: 'トップページのサブタイトルです',
+        topFeature1: 'トップページの機能１です',
+        topFeature2: 'トップページの機能２です'
+    };
+    console.log(labels);
+    // 値部分の型が合わないためエラー
+    // const hoge: Label = {
+    //   message: 100
+    // }
 }
-printPoint({ x: 100, y: 200 });
-// 型が合っていてもプロパティ名が異なるとエラーになる
-// printPoint({ z: 100, t: 200});
+kataAriase();
+// インターフェース 
+function iinterface() {
+    // interface 型名 {
+    //   プロパティ１: 型１;
+    //   プロパティ２: 型２;
+    // }
+    function printPoint(point) {
+        console.log("x\u5EA7\u6A19\u306F ".concat(point.x, " \u3067\u3059"));
+        console.log("y\u5EA7\u6A19\u306F ".concat(point.y, " \u3067\u3059"));
+        console.log("z\u5EA7\u6A19\u306F ".concat(point.z, " \u3067\u3059"));
+    }
+    // 引数のオブジェクトにzが存在しないためコンパイル時にエラーになる
+    // printPoint({ x: 100, y: 200 });
+    // 問題なく動作します
+    printPoint({ x: 100, y: 200, z: 300 });
+    var MyPoint = /** @class */ (function () {
+        function MyPoint() {
+        }
+        return MyPoint;
+    }());
+    var cc = {
+        color: '赤',
+        radius: 10
+    };
+}
+iinterface();
+function klass() {
+    // class クラス名 {
+    //   フィールド1: 型1;
+    //   フィールド2: 型2;
+    // }
+    var Point = /** @class */ (function () {
+        // 引数がない場合の初期値を指定（コンストラクタ)
+        function Point(x, y) {
+            if (x === void 0) { x = 0; }
+            if (y === void 0) { y = 0; }
+            this.x = x;
+            this.y = y;
+        }
+        // 戻り値がない関数を定義するためにvoidを指定する
+        Point.prototype.moveX = function (n) {
+            this.x += n;
+        };
+        Point.prototype.moveY = function (n) {
+            this.y += n;
+        };
+        return Point;
+    }());
+    var point = new Point();
+    point.moveX(10);
+    console.log("".concat(point.x, ", ").concat(point.y)); // 10, 0
+    // extends
+    var Point3D = /** @class */ (function (_super) {
+        __extends(Point3D, _super);
+        function Point3D(x, y, z) {
+            if (x === void 0) { x = 0; }
+            if (y === void 0) { y = 0; }
+            if (z === void 0) { z = 0; }
+            // 継承元のコンストラクタを呼び出す
+            var _this = _super.call(this, x, y) || this;
+            _this.z = z;
+            return _this;
+        }
+        Point3D.prototype.moveZ = function (n) {
+            this.z += n;
+        };
+        return Point3D;
+    }(Point));
+    var point3D = new Point3D();
+    // 継承元のメソッドを呼び出すことができる
+    point3D.moveX(20);
+    point3D.moveZ(50);
+    console.log("".concat(point3D.x, ", ").concat(point3D.y, ", ").concat(point3D.z)); // 20, 0, 50
+    var User = /** @class */ (function () {
+        function User() {
+            this.name = '';
+            this.age = 0;
+        }
+        // インターフェースに定義されているメソッドを実装しないと
+        // コンパイル時にエラーになる
+        User.prototype.sayHello = function () {
+            return "\u3053\u3093\u306B\u3061\u306F\u3001\u79C1\u306F".concat(this.name, "\u3001").concat(this.age, "\u6B73\u3067\u3059");
+        };
+        return User;
+    }());
+    var user = new User();
+    user.name = 'Yuhei';
+    user.age = 28;
+    console.log(user.sayHello());
+    // アクセス修飾子
+    var BasePoint3D = /** @class */ (function () {
+        function BasePoint3D() {
+        }
+        return BasePoint3D;
+    }());
+    // インスタンス化を行なった場合のアクセス制御の例
+    var basePoint = new BasePoint3D();
+    basePoint.x; // OK
+    // basePoint.y: // コンパイルエラー
+    // basePoint.z; // コンパイルエラー
+    var ChildPoint = /** @class */ (function (_super) {
+        __extends(ChildPoint, _super);
+        function ChildPoint() {
+            var _this = _super.call(this) || this;
+            _this.x; // OK
+            // this.y; // コンパイルエラー
+            _this.z; // OK
+            return _this;
+        }
+        return ChildPoint;
+    }(BasePoint3D));
+}
+klass();
--- src/2-3.ts
+++ src/2-3.ts
@@ -64,21 +64,230 @@ function kataAssertion() {
 
 kataAssertion();
 
-// 型エイリアス
-// type 型名 = 型
-type Name = string;
+function kataAriase() {
 
-// xとyの座標プロパティを持つPointという型エイリアスを定義する例
-type Point = {
-  x: number;
-  y: number;
+  // 型エイリアス
+  // type 型名 = 型
+  type Name = string;
+  
+  // xとyの座標プロパティを持つPointという型エイリアスを定義する例
+  type Point = {
+    x: number;
+    y: number;
+  }
+  
+  function printPoint(point: Point) {
+    console.log(`x座標は ${point.x} です`);
+    console.log(`y座標は ${point.y} です`);
+  }
+  
+  printPoint({ x: 100, y: 200 });
+  // 型が合っていてもプロパティ名が異なるとエラーになる
+  // printPoint({ z: 100, t: 200});
+  
+  // 関数自体の型もエイリアスとして定義できる
+  type Formatter = (a: string) => string;
+  
+  function printName(firstName: string, formatter: Formatter) {
+    console.log(formatter(firstName));
+  }
+
+  const upperCaseFormatter: Formatter = (name: string) => name.toUpperCase();
+
+  printName('Michinobu', upperCaseFormatter);
+
+  // オブジェクトのキー名を明記せずに型エイリアスを定義できる
+  // { [] : 型名 }
+  type Label = {
+    [key: string]: string;
+  }
+
+  // 上記エイリアスを指定して、
+  // 画面に表示する文字を定義するオブジェクトのキーと値を以下のように定義できる
+  const labels: Label = {
+    topTitle: 'トップページのタイトルです',
+    topSubTitle: 'トップページのサブタイトルです',
+    topFeature1: 'トップページの機能１です',
+    topFeature2: 'トップページの機能２です'
+  }
+
+  console.log(labels);
+
+  // 値部分の型が合わないためエラー
+  // const hoge: Label = {
+  //   message: 100
+  // }
 }
 
-function printPoint(point: Point) {
-  console.log(`x座標は ${point.x} です`);
-  console.log(`y座標は ${point.y} です`);
+kataAriase();
+
+// インターフェース 
+function iinterface() {
+  // interface 型名 {
+  //   プロパティ１: 型１;
+  //   プロパティ２: 型２;
+  // }
+
+  interface Point {
+    x: number;
+    y: number;
+  }
+
+  function printPoint(point: Point) {
+    console.log(`x座標は ${point.x} です`);
+    console.log(`y座標は ${point.y} です`);
+    console.log(`z座標は ${point.z} です`);
+  }
+
+  // このようにインターフェースは後から拡張可能
+  interface Point {
+    z: number;
+  }
+
+  // 引数のオブジェクトにzが存在しないためコンパイル時にエラーになる
+  // printPoint({ x: 100, y: 200 });
+
+  // 問題なく動作します
+  printPoint({ x: 100, y: 200, z: 300 });
+
+  // クラスがインターフェースをimplementsした際に、zが存在しないため
+  // コンパイル時にエラーになる
+  // class MyPoint implements Point {
+  //   x: number;
+  //   y: number;
+  // }
+
+  interface Point2 {
+    x: number;
+    y: number;
+    z?: number; // オプショナルなプロパティ
+  }
+
+  class MyPoint implements Point2 {
+    x: number;
+    y: number;
+  }
+
+  // extends
+  interface Coloful {
+    color: string;
+  }
+
+  interface Circle {
+    radius: number;
+  }
+  // 複数のインターフェースを継承して新たなインターフェースを定義できる
+  interface ColofulCircle extends Coloful, Circle {}
+
+  const cc: ColofulCircle = {
+    color: '赤',
+    radius: 10
+  }
 }
 
-printPoint({ x: 100, y: 200 });
-// 型が合っていてもプロパティ名が異なるとエラーになる
-// printPoint({ z: 100, t: 200});
+iinterface();
+
+function klass() {
+  // class クラス名 {
+  //   フィールド1: 型1;
+  //   フィールド2: 型2;
+  // }
+
+  class Point {
+    x: number;
+    y: number;
+
+    // 引数がない場合の初期値を指定（コンストラクタ)
+    constructor(x: number = 0, y: number = 0) {
+      this.x = x;
+      this.y = y;
+    }
+
+    // 戻り値がない関数を定義するためにvoidを指定する
+    moveX(n: number): void {
+      this.x += n;
+    }
+
+    moveY(n: number): void {
+      this.y += n;
+    }
+  }
+
+  const point = new Point();
+  point.moveX(10);
+  console.log(`${point.x}, ${point.y}`); // 10, 0
+
+  // extends
+  class Point3D extends Point {
+    z: number;
+
+    constructor(x: number = 0, y: number = 0, z: number = 0) {
+      // 継承元のコンストラクタを呼び出す
+      super(x, y);
+      this.z = z;
+    }
+
+    moveZ(n: number): void {
+      this.z += n;
+    }
+  }
+
+  const point3D = new Point3D();
+  // 継承元のメソッドを呼び出すことができる
+  point3D.moveX(20);
+  point3D.moveZ(50);
+  console.log(`${point3D.x}, ${point3D.y}, ${point3D.z}`); // 20, 0, 50
+
+  // インターフェースに対してimplementsを利用することで
+  // クラスに対する実装の強制が可能
+  interface IUser {
+    name: string;
+    age: number;
+    sayHello: () => string; // 引数なしで文字列を返す
+  }
+
+  class User implements IUser {
+    name: string;
+    age; number;
+
+    constructor() {
+      this.name = '';
+      this.age = 0;
+    }
+
+    // インターフェースに定義されているメソッドを実装しないと
+    // コンパイル時にエラーになる
+    sayHello(): string {
+      return `こんにちは、私は${this.name}、${this.age}歳です`;
+    }
+  }
+
+  const user = new User();
+  user.name = 'Yuhei';
+  user.age = 28;
+  console.log(user.sayHello());
+
+  // アクセス修飾子
+  class BasePoint3D {
+    public x: number;
+    private y: number;
+    protected z: number;
+  }
+
+  // インスタンス化を行なった場合のアクセス制御の例
+  const basePoint = new BasePoint3D();
+  basePoint.x; // OK
+  // basePoint.y: // コンパイルエラー
+  // basePoint.z; // コンパイルエラー
+
+  class ChildPoint extends BasePoint3D {
+    constructor() {
+      super();
+      this.x; // OK
+      // this.y; // コンパイルエラー
+      this.z; // OK
+    }
+  }
+}
+
+klass();
